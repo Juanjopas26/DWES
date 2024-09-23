11ba7b3c306c869386e719b616c40d69bf975f09
juanjopas26 <juanjose.pascualvaca@iesdonana.org>
2024-09-23 20:27:29 +0200

    Inicio

---
+++ ficha.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+    
+    <?php 
+    $x = 25;
+    $y = 30;
+    $z = $x + $y; 
+    ?>
+    <p> Esto es el resultado del formulario. </p>
+</body>
+</html>
